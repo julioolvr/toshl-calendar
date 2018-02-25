04a6d295faf91a906b7a9fb2bc850f93cf9e4195
Julio Olivera <julio.olvr@gmail.com>
2018-02-24 22:18:55 -0300

    Return expenses that are on repeat

--- app.rb
+++ app.rb
@@ -6,8 +6,16 @@ get '/cal.ical' do
   token = params['token']
   # TODO: Fail if token missing
 
-  resp = Faraday.new(url: 'https://api.toshl.com/me') do |faraday|
+  resp = Faraday.new(url: 'https://api.toshl.com/entries') do |faraday|
     faraday.basic_auth(token, nil)
     faraday.adapter  Faraday.default_adapter
+    faraday.params[:expand] = false
+    faraday.params[:per_page] = 500
+    faraday.params[:type] = :expense
+    faraday.params[:from] = '2018-01-01'
+    faraday.params[:to] = '2019-01-01'
   end.get.body
+  resp = JSON.parse(resp).select { |entry| entry.has_key?('repeat') }
+
+  JSON.dump resp
 end
