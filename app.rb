0df7b9c776510f6cecac84a23f00f53c76cdc762
Julio Olivera <julio.olvr@gmail.com>
2018-02-25 14:44:44 -0300

    Added Loggly integration

--- app.rb
+++ app.rb
@@ -1,13 +1,30 @@
+require 'dotenv/load'
+
 require 'sinatra'
+require 'sinatra/custom_logger'
+require 'logglier'
+
 require './lib/client'
 require './lib/calendar'
+
+if production? && ENV.has_key?('LOGGLY_TOKEN')
+  set :logger,
+      Logglier.new(
+        "https://logs-01.loggly.com/inputs/#{ENV['LOGGLY_TOKEN']}/tag/ruby/",
+        format: :json,
+        threaded: true
+      )
+end
 
 get '/' do
   send_file File.expand_path('index.html', settings.public_folder)
 end
 
 get '/cal' do
-  halt 422, 'Missing token parameter' unless params.has_key?('token')
+  unless params.has_key?('token')
+    logger.info 'Tried to fetch /cal without a token'
+    halt 422, 'Missing token parameter'
+  end
 
   content_type 'text/calendar'
   token = params['token']
