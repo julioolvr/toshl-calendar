68fb9f3c4c33beb7273abd12d20802a19c29ceee
Julio Olivera <julio.olvr@gmail.com>
2018-02-24 21:23:18 -0300

    Pass through request with personal token to Toshl's API

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'faraday'
 
-get '/' do
-  'Hello world'
+get '/cal.ical' do
+  content_type :json
+  token = params['token']
+  # TODO: Fail if token missing
+
+  resp = Faraday.new(url: 'https://api.toshl.com/me') do |faraday|
+    faraday.basic_auth(token, nil)
+    faraday.adapter  Faraday.default_adapter
+  end.get.body
 end
