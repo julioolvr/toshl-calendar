615adc6baab2e989bb339240b538516767061ee6
Julio Olivera <julio.olvr@gmail.com>
2018-02-25 01:34:16 -0300

    Basic landing page

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require './lib/client'
 require './lib/calendar'
 
+get '/' do
+  send_file File.expand_path('index.html', settings.public_folder)
+end
+
 get '/cal' do
   halt 422, 'Missing token parameter' unless params.has_key?('token')
 
