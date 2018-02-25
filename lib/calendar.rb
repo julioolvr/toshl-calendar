5c3efa1958e04a6c56db2347ca3931ee2016b014
Julio Olivera <julio.olvr@gmail.com>
2018-02-24 23:34:24 -0300

    Added recurrence rules

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -12,6 +12,7 @@ class Calendar
       icalendar.event do |event|
         event.dtstart = Icalendar::Values::Date.new(entry.date.strftime('%Y%m%d'))
         event.summary = entry.description
+        event.rrule = Icalendar::Values::Recur.new(entry.recur)
       end
     end
 
--- lib/entry.rb
+++ lib/entry.rb
@@ -9,6 +9,18 @@ class Entry
     Date.strptime(data['date'], '%Y-%m-%d')
   end
 
+  def recur
+    repeat_data = @data['repeat']
+
+    builder = ["FREQ=#{repeat_data['frequency']}", "INTERVAL=#{repeat_data['interval']}"]
+    builder << "COUNT=#{repeat_data['count']}" if repeat_data.has_key?('count')
+    builder << "BYDAY=#{repeat_data['byday']}" if repeat_data.has_key?('byday')
+    builder << "BYMONTHDAY=#{repeat_data['bymonthday']}" if repeat_data.has_key?('bymonthday')
+    builder << "BYSETPOS=#{repeat_data['bysetpos']}" if repeat_data.has_key?('bysetpos')
+
+    builder.join(';')
+  end
+
   def description
     data_description = data['desc'].strip
 
